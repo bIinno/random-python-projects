1b7baf8d14c7784b88e04b989408840d558c330f
blinno <56482897+bIinno@users.noreply.github.com>
2020-09-23 19:53:20 +0100

    Simple file writing script

--- writefile.py
+++ writefile.py
@@ -1,4 +1,3 @@
-
 s = input("Writefile text: \n")
 print("Your text has been written to a file named \"writefile.txt\" and should be found in the same directory as this application!")
 f = open('writefile.txt', 'w')
