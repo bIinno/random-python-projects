df98875d88421ded75f7a8c7d9aaf44c99f06444
blinno <56482897+bIinno@users.noreply.github.com>
2020-09-23 20:00:07 +0100

    Calculates age
    Doesn't even use any fancy packages or anything just simple built-in maths thing in python.

---
+++ agecalc.py
@@ -0,0 +1,10 @@
+currentyear = 2020
+# or you could set current year as an input as well to
+# ^^ keep this program relevant for all of eternity-
+
+# currentyear = input("Current Year: ")
+birthyear = input("Birth Year: ")
+
+result = int(currentyear) - int(birthyear)
+
+print(result)
